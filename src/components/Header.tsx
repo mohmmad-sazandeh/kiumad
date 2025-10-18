cd3bd978c21ca64f1659e57c6b4dd25f241fd4f9
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-10-18 11:13:21 +0330

    add button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header({ onAddProduct }: HeaderProps) {
           اضافه کردن
         </button>
         <button className="border border-gray-500 rounded-md px-4 py-2 text-white hover:bg-gray-700">
-          -
+          انبار
         </button>
         <button className="border border-gray-500 rounded-md px-4 py-2 text-white hover:bg-gray-700">
           -
