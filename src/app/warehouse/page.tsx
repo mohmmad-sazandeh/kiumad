02d3f0e8624cc6c57e478d09f0ab06d86cb62b00
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-10-18 17:31:50 +0330

    Update page.tsx

--- src/app/warehouse/page.tsx
+++ src/app/warehouse/page.tsx
@@ -1,4 +1,3 @@
-// src/app/warehouse/page.tsx  (یا app/warehouse/page.tsx)
 export default function WarehousePage() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 p-8" dir="rtl">
@@ -6,10 +5,9 @@ export default function WarehousePage() {
 
       <div className="bg-white shadow-md rounded-lg p-6">
         <p className="text-gray-700">
-          این صفحهٔ انبار است. در آینده اینجا لیست موجودی، جزئیات هر کالا و فیلترها نمایش داده می‌شود.
+            لیست محصولات
         </p>
 
-        {/* نمونه کارت ساده */}
         <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           <div className="border rounded-md p-4">
             <p className="font-bold">نام محصول : مثال</p>
@@ -21,3 +19,4 @@ export default function WarehousePage() {
     </div>
   );
 }
+
